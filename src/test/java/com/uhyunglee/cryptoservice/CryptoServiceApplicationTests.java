4050fff052ae76648a2d41a1eb17be6cc40671c1
uhyunglee <uhyunglee@naver.com>
2024-04-12 15:19:24 +0900

    [FEAT] : Project Setting

---
+++ src/test/java/com/uhyunglee/cryptoservice/CryptoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uhyunglee.cryptoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
